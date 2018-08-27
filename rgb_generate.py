792926ebc9cb1218d4ec29be85b2805c93bbcc00
ThomasWalter <thomas.walter@mines-paristech.fr>
2018-08-27 11:06:35 +0200

    rgb_generate is removed. This task is now fulfilled by process_ilastik.py

--- rgb_generate.py
+++
@@ -1,6 +0,0 @@
-from sequence_importer import RGB_Generator
-
-rg = RGB_Generator(channel_order=['CD3', 'CD19'])
-rg.batch_processing('/Users/twalter/data/Elodie/Ilastik/channels',
-                    '/Users/twalter/data/Elodie/Ilastik/data/rgb',
-                    tissue_ids = ['Tonsil_D1', 'Tonsil_D2'])
