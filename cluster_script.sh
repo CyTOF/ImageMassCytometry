93c0990a15025da19e685ff4dc49eed5c6e7b296
ThomasWalter <thomas.walter@mines-paristech.fr>
2018-09-24 05:28:11 +0200

    insignificant change

--- cluster_script.sh
+++ cluster_script.sh
@@ -6,11 +6,11 @@
 export CLUSTER_ENV=True
 
 python clustering.py -s ./ims_2018_09_13.py --tissue_id Tonsil_D2 --hierarchical_clustering --nb_clusters 60 --cluster_galleries --cluster_maps --distance euclidean --method ward
-python clustering.py -s ./ims_2018_09_13.py --tissue_id Tonsil_D1 --hierarchical_clustering --nb_clusters 60 --cluster_galleries --cluster_maps --distance euclidean --method ward
+#python clustering.py -s ./ims_2018_09_13.py --tissue_id Tonsil_D1 --hierarchical_clustering --nb_clusters 60 --cluster_galleries --cluster_maps --distance euclidean --method ward
 
 python clustering.py -s ./ims_2018_09_13.py --tissue_id Tonsil_D2 --hierarchical_clustering --nb_clusters 60 --cluster_galleries --cluster_maps --distance correlation --method average
-python clustering.py -s ./ims_2018_09_13.py --tissue_id Tonsil_D1 --hierarchical_clustering --nb_clusters 60 --cluster_galleries --cluster_maps --distance correlation --method average
+#python clustering.py -s ./ims_2018_09_13.py --tissue_id Tonsil_D1 --hierarchical_clustering --nb_clusters 60 --cluster_galleries --cluster_maps --distance correlation --method average
 
 python clustering.py -s ./ims_2018_09_13.py --tissue_id Tonsil_D2 --hierarchical_clustering --nb_clusters 60 --cluster_galleries --cluster_maps --distance cosine --method average
-python clustering.py -s ./ims_2018_09_13.py --tissue_id Tonsil_D1 --hierarchical_clustering --nb_clusters 60 --cluster_galleries --cluster_maps --distance cosine --method average
+#python clustering.py -s ./ims_2018_09_13.py --tissue_id Tonsil_D1 --hierarchical_clustering --nb_clusters 60 --cluster_galleries --cluster_maps --distance cosine --method average
 
