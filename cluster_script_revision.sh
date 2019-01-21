c9ba9dc925eddceb53aa7716765fc5c766813f74
ThomasWalter <thomas.walter@mines-paristech.fr>
2019-01-21 18:28:34 +0100

    cluster adaptation

---
+++ cluster_script_revision.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --mem 80000 # memory pool for all cores
+#SBATCH -o slurm.%N.%j.out # STDOUT
+#SBATCH -e slurm.%N.%j.err # STDERR
+
+export CLUSTER_ENV=True
+
+#python clustering.py -s ./ims_2019_01_16_cluster.py --tissue_id Tonsil_D2 --hierarchical_clustering --nb_clusters 60  --distance euclidean --method ward
+python clustering.py -s ./ims_2019_01_16_cluster.py --tissue_id Tonsil_D2 --hierarchical_clustering --nb_clusters 60  --distance euclidean --method ward --load_clustering --cluster_maps --cluster_galleries
+
+#python clustering.py -s ./ims_2018_09_13.py --tissue_id Tonsil_D2 --hierarchical_clustering --nb_clusters 60  --distance euclidean --method ward
+#python clustering.py -s ./ims_2018_09_13.py --tissue_id Tonsil_D1 --hierarchical_clustering --nb_clusters 60 --cluster_galleries --cluster_maps --distance euclidean --method ward
+
+#python clustering.py -s ./ims_2018_09_13.py --tissue_id Tonsil_D2 --hierarchical_clustering --nb_clusters 60  --distance correlation --method average
+#python clustering.py -s ./ims_2018_09_13.py --tissue_id Tonsil_D1 --hierarchical_clustering --nb_clusters 60 --cluster_galleries --cluster_maps --distance correlation --method average
+
+#python clustering.py -s ./ims_2018_09_13.py --tissue_id Tonsil_D2 --hierarchical_clustering --nb_clusters 60  --distance cosine --method average
+#python clustering.py -s ./ims_2018_09_13.py --tissue_id Tonsil_D1 --hierarchical_clustering --nb_clusters 60 --cluster_galleries --cluster_maps --distance cosine --method average
+
